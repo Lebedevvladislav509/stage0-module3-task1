aea0ec3e73d54419551821a8f69013fb54c4145f
LVLAD2009@yandex.ru <24233lebedko>
2023-03-21 20:52:16 +0300

    0 stage, 3rd module

--- src/main/java/lang/print/gaps/task1/PrintWordsApp.java
+++ src/main/java/lang/print/gaps/task1/PrintWordsApp.java
@@ -3,6 +3,9 @@ package lang.print.gaps.task1;
 public class PrintWordsApp {
     public static void main(String[] args) {
         //write your code here
-        System.out.print("java" + "\n" + "is" + "\n" + "smth");
+        System.out.print("""
+                java
+                is
+                smth""");
     }
 }
